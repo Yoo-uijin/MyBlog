78dd508c74781efde145be5891bc0474745d6da3
Yoo-uijin <timetam24@naver.com>
2023-05-01 05:04:58 +0900

    update

--- my-app/src/components/Phrase.js
+++ my-app/src/components/Phrase.js
@@ -1,14 +1,16 @@
 import React from "react";
 
 const Phrase = ({ phraseList }) => {
+  const randomNum = parseInt(Math.random() * phraseList.length + 1);
+
+  const randomPhrase = phraseList.filter(
+    (item) => parseInt(item.id) === randomNum
+  );
+
   return (
     <div className="Phrase">
-      <h2 className="Phrase_text">
-        {phraseList[Math.floor(Math.random() * phraseList.length)].text}
-      </h2>
-      <h3 className="Phrase_writer">
-        {phraseList[Math.floor(Math.random() * phraseList.length)].writer}
-      </h3>
+      <h2 className="Phrase_text">{randomPhrase[0].text}</h2>
+      <h3 className="Phrase_writer">{randomPhrase[0].writer}</h3>
     </div>
   );
 };
